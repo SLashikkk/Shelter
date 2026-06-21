945a1a92abc35f0819e0be2812e20cc9891cf3a4
Maksim Safonov <stmslashikk@mail.ru>
2026-06-21 11:58:48 +0300

    feat: render cards dynamically using template clone and loop through pets data

--- data/getData.js
+++ data/getData.js
@@ -8,7 +8,23 @@ try {
 
   const data = await response.json();
   const petsData = data.map((item, index) => ({ id: index, ...item }));
+  const sliderLine = document.querySelector(".our-friends__slider-track");
+  const template = document.getElementById("pet__card__template");
+  if (sliderLine && template) {
+    sliderLine.innerHTML = "";
+    petsData.forEach((pet) => {
+      const cardClone = template.content.cloneNode(true);
+      const img = cardClone.querySelector(".card__img");
+      const name = cardClone.querySelector(".card__name");
+      const button = cardClone.querySelector(".card__button");
 
+      name.textContent = pet.name;
+      const filename = pet.img.split("/").pop();
+      img.src = `./img/pets-${filename}`;
+      img.alt = pet.name;
+      sliderLine.appendChild(cardClone);
+    });
+  }
 } catch (error) {
   console.error("Error fetching the pets data:", error);
 }
