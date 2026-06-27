8920a224a906e94e7b347cf995b1ec6ebb87377b
Maksim Safonov <stmslashikk@mail.ru>
2026-06-27 20:46:03 +0300

    feat: select DOM elements for popup

--- js/modules/popup.js
+++ js/modules/popup.js
@@ -1,4 +1,14 @@
 export function openPopup(pet) {
+  const overlay = document.querySelector(".popup__overlay");
+  if (!overlay || !pet) return;
 
-  
+  const img = overlay.querySelector(".popup__img");
+  const name = overlay.querySelector(".popup__name");
+  const typeBreed = overlay.querySelector(".popup__type__breed");
+  const description = overlay.querySelector(".popup__description");
+  const age = overlay.querySelector(".popup__age");
+  const inoculations = overlay.querySelector(".popup__inoculations");
+  const diseases = overlay.querySelector(".popup__diseases");
+  const parasites = overlay.querySelector(".popup__parasites");
 }
+
