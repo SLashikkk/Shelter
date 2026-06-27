a20ae511d6eaf47941b558d58b9de8a53c2987a9
Maksim Safonov <stmslashikk@mail.ru>
2026-06-27 20:11:02 +0300

    feat: add popup layout and css styles

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 import { initBurger } from "./modules/burger.js";
 import { initSlider } from "./modules/slider.js";
+import { openPopup } from "./modules/popup.js";
 
 initBurger();
 initSlider();
-
-
+openPopup();
