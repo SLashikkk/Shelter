86e75e685c849a29aba61f06b626e8ed622195e1
Maksim Safonov <stmslashikk@mail.ru>
2026-06-28 23:07:42 +0300

    fix: remove lowercase conversion to match github case-sensitive filenames

--- js/modules/popup.js
+++ js/modules/popup.js
@@ -20,7 +20,7 @@ export function openPopup(pet) {
   parasites.textContent = pet.parasites.join(", ");
 
   name.textContent = pet.name;
-  const filename = pet.img.split("/").pop().toLowerCase();
+  const filename = pet.img.split("/").pop();
   img.src = `img/pets-${filename}`;
   img.alt = pet.name;
 
